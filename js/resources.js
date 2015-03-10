d07de8fbb7f565f7abe9902911a2315e23dcae6c
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-10 22:02:44 +0000

    added resources.js

--- js/resources.js
+++ js/resources.js
@@ -1,17 +1,21 @@
 game.resources = [
-    {
-        "name": "area01_level_tiles",
-        "type": "image",
-        "src": "data/img/map/area01_level_tiles.png"
-    },
-    {
-        "name": "gripe_run_right",
-        "type": "image",
-        "src": "data/img/sprite/gripe_run_right.png"
-    },
-    {
-        "name": "area01",
-        "type": "tmx",
-        "src": "data/map/area01.tmx"
-    }
+	/**
+	 * Graphics.
+	 */
+	// our level tileset
+	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+	// the main player spritesheet
+	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
+	/* 
+	 * Maps. 
+ 	 */
+	{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"}
+
+	/* 
+	 * Background music. 
+	 */	
+	
+	/* 
+	 * Sound effects. 
+	 */
 ];
